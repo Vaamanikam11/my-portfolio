358b51ccc5f8dff7c5ac0213c741f30253b95105
Vaama Nikam <vaamanikam11@gmail.com>
2024-07-08 22:14:46 +0530

    Change 2

--- src/components/about.js
+++ src/components/about.js
@@ -12,10 +12,9 @@ const About = () => {
         <div className="about-content">
           <h1 style={{ color: '#333' }}>About me</h1>
           <p>
-            Hello! I'm Vaama Nikam, a Computer Engineer with a Master's degree in Computer Science from Syracuse University.
-            Throughout my academic journey, I have focused on developing my skills in AI, including machine learning algorithms, 
-            data analysis, and natural language processing. I have also been involved in various research projects related to AI, 
-            including developing chatbots and analyzing large datasets using advanced algorithms.
+          Hello! I'm Vaama Nikam, a Computer Engineer with a Master's degree in Computer Science from Syracuse University. Throughout my academic journey, 
+          I have focused on developing my skills in AI, including machine learning algorithms, data analysis, and natural language processing. I have also 
+          been involved in various research projects related to AI, including developing chatbots and analyzing large datasets using advanced algorithms.
           </p>
           <p>
           In addition to my academic pursuits, I have participated in several AI-related competitions and hackathons, where I have 
@@ -24,28 +23,29 @@ const About = () => {
           in the field. I am eager to connect with industry professionals and continue to learn and grow as a professional in this 
           exciting field.
           </p>
+          <p>I am currently looking for Fall 2024 co-ops/internships and Summer 2025 new grad full-time roles.</p>
           <div className="skills-tools">
             <div className="section">
               <h3>Skills</h3>
               <ul>
-                <li>Programming Languages: C, C++, Java, Python, SQL</li>
-                <li>Web: HTML, CSS3, JavaScript, jQuery, Bootstrap, PHP, Express, Angular, Node.js</li>
-                <li>Analytics and ML: NumPy, Pandas, TensorFlow, Scikit-learn, Keras</li>
+                <li>Programming Languages: C, C++, Python, SQL</li>
+                <li>Web: HTML, CSS3, JavaScript, jQuery, Bootstrap, React, React Native</li>
+                <li>Analytics and ML: NumPy, Pandas, TensorFlow, Scikit-learn, Keras, Computer Vision, Generative AI, Artificial Intelligence, NLP, LLM</li>
               </ul>
             </div>
             <div className="section">
               <h3>Tools</h3>
               <ul>
                 <li>Database Tools: MySQL, MongoDB</li>
-                <li>Visualization Tools: Tableau, Python Libraries</li>
-                <li>Automation: Selenium</li>
-                <li>Mobile app development: Dart, Flutter, Android app development</li>
+                <li>Visualization Tools: Tableau, Python Libraries, Power BI</li>
+                <li>Git Version Control, GitHub</li>
+                
               </ul>
             </div>
             <div className="section">
               <h3>Software</h3>
               <ul>
-                <li>BlueJ, PyCharm, Jupyter, MATLAB, Android Studio, VS Code, AutoCAD, Microsoft Office</li>
+                <li>Anaconda, PyCharm, Jupyter, Android Studio, VS Code, XCode, Windows, UNIX, MacOS, Microsoft Office</li>
               </ul>
             </div>
           </div>
--- src/components/home.js
+++ src/components/home.js
@@ -1,43 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Typewriter, Cursor } from 'react-simple-typewriter';
 import { Container, Row, Col } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from '../../src/Cover.png'; // Update the path if necessary
+import headerImg from '../../src/Cover.png';
 import './home.css';
-import { Typed } from 'react-typed';
-
-// const Home = () => {
-//   const roles = ['Software Developer', 'AI/ML Enthusiast', 'Data Scientist'];
-//   const [currentRole, setCurrentRole] = useState(0);
-
-//   useEffect(() => {
-//     const roleChangeInterval = setInterval(() => {
-//       setCurrentRole((prevRole) => (prevRole + 1) % roles.length);
-//     }, 2000); // Change role every 2 seconds
-//     return () => clearInterval(roleChangeInterval);
-//   }, [roles.length]);
-
-//   return (
-//     <section className="home" id="home">
-//       <div className="home-overlay">
-//         <Container>
-//           <Row className="align-items-center">
-//             <Col xs={12} md={6} xl={7}>
-//               <span className="tagline">Welcome to my Portfolio</span>
-//               <h1>
-//                 Hi! I am Vaama a <span className="wrap">{roles[currentRole]}</span>
-//               </h1>
-//               {/* <button onClick={() => console.log('connect')}>
-//                 Resume <ArrowRightCircle size={25} />
-//               </button> */}
-//             </Col>
-//           </Row>
-//         </Container>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Home;
 
 const Home = () => {
   const roles = ['Software Developer', 'AI/ML Enthusiast', 'Data Scientist'];
@@ -45,32 +10,22 @@ const Home = () => {
   return (
     <section className="home" id="home">
       <div className="home-overlay">
-        <Container>
-          <Row className="align-items-center">
-            <Col xs={12} md={6} xl={7}>
-              <span className="tagline">Welcome to my Portfolio</span>
-              <h1>
-                Hi! I am Vaama{' '}
-                <span className="wrap">
-                  <Typed
-                    strings={roles}
-                    typeSpeed={50}
-                    backSpeed={30}
-                    loop
-                  />
-                </span>
-              </h1>
-              <p>
-                <a href="#contact" className="btn btn-primary">
-                  Resume
-                </a>
-              </p>
-            </Col>
-            <Col xs={12} md={6} xl={5}>
-              <img src={headerImg} alt="Header Img" />
-            </Col>
-          </Row>
-        </Container>
+        <span className="tagline">Welcome to my Portfolio</span>
+        <h1>
+          Hi! I am Vaama a {' '}
+          <span className="wrap">
+            <Typewriter
+              words={roles}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </h1>
+        
       </div>
     </section>
   );
